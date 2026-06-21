80c290d0121b75050a82f31326132886cf8c50a7
92mimx2-tech <92mimx2@gmail.com>
2026-06-21 21:31:13 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,31 +1,31 @@
-```javascript
 const weddingInfo = {
 
-groom: "송태빈",
+    groom: "송태빈",
 
-bride: "정다운",
+    bride: "정다운",
 
-date: "2026.12.05",
+    date: "2026.12.05",
 
-time: "오전 10시",
+    day: "토요일",
 
-hall: "안양 더파티움 라포레홀 7층",
+    time: "오전 10시",
 
-verse: "너희 모든 일을 사랑으로 행하라",
+    hall: "안양 더파티움 라포레홀 7층",
 
-verseRef: "고린도전서 16:14",
+    verse: "너희 모든 일을 사랑으로 행하라",
 
-groomPhone: "01011112222",
+    verseRef: "고린도전서 16:14",
 
-bridePhone: "01033334444",
+    groomPhone: "01011112222",
 
-groomAccountBank: "국민은행",
+    bridePhone: "01033334444",
 
-groomAccountNumber: "123456-78-123456",
+    groomBank: "국민은행",
 
-brideAccountBank: "신한은행",
+    groomAccount: "123456-78-123456",
 
-brideAccountNumber: "110-111-111111"
+    brideBank: "신한은행",
+
+    brideAccount: "110-111-111111"
 
 };
-```
